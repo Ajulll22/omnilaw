415dc8167b2b4f9ef6cb56736afffe958dd7d6f0
Ajulll22 <ajulrizki@gmail.com>
2022-06-24 15:07:42 +0700

    controller login

---
+++ application/controllers/Login.php
@@ -0,0 +1,47 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Login extends CI_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('m_login');
+    }
+
+    public function index()
+    {
+        if (!empty($this->session->userdata('loginData'))) {
+            redirect('admin/kelola/');
+        }
+
+        $data['project_name'] = "Putra Arsip";
+        $data['established'] = "2020";
+        $this->load->view('member/Login', $data);
+    }
+
+    public function doLogin()
+    {
+        $dataPost = $this->input->post();
+        $login = $this->m_login->checkLogin($dataPost['username'], md5($dataPost['password']));
+        if ($login) {
+            if ($this->session->userdata['loginData']['Level'] == "verifikator") {
+                redirect('admin/pasal/');
+            } elseif ($this->session->userdata['loginData']['Level'] == "admin") {
+                redirect('admin/kelola/');
+            }
+        } else {
+            $this->session->set_flashdata('GagalLogin', 'Ya');
+            redirect('/');
+        }
+    }
+
+
+
+    function log()
+    {
+        $this->session->unset_userdata('loginData');
+        redirect('/');
+    }
+}
