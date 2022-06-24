87b0bd478a14cf81458cb25dcf88d3018f646a82
Ajulll22 <ajulrizki@gmail.com>
2022-06-24 15:09:13 +0700

    controller dashboard admin

---
+++ application/controllers/admin/Dashboard.php
@@ -0,0 +1,23 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+    function __construct() {
+        parent::__construct();
+        $this->load->model('m_umum');
+    }
+
+
+    public function index()
+	{
+		$data['userLogin'] 			= $this->session->userdata('loginData');
+		$data['v_content'] 			= 'member/dashboard/content';//ngambil view
+		$this->load->view('member/layout',$data);
+	}
+    
+	function decryptIt( $q ) {
+		$cryptKey  = 'qJB0rGtIn5UB1xG03efyCp';
+		$qDecoded      = rtrim( mcrypt_decrypt( MCRYPT_RIJNDAEL_256, md5( $cryptKey ), base64_decode( $q ), MCRYPT_MODE_CBC, md5( md5( $cryptKey ) ) ), "\0");
+		return( $qDecoded );
+	}
+       
+}
